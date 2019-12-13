c8d82dacc9a1a41679ab52df6943a3c5f4c86c74
Victor Graffard <victor.graffard@iadvize.com>
2019-12-13 14:46:15 +0100

    feat(selectors): Now using reselect

--- src/selector.ts
+++ src/selector.ts
@@ -1,48 +1,39 @@
+import { createSelector } from 'reselect';
+
 import { pipe } from 'fp-ts/lib/pipeable';
 import { map, filter } from 'fp-ts/lib/Array';
-import { fromNullable, Option, map as mapOption } from 'fp-ts/lib/Option';
+import { fromNullable, map as mapOption } from 'fp-ts/lib/Option';
 
 import { Movie, State, Actor } from "./entities";
 
-export const movies = (state: State): State['movies'] => state.movies;
+export const movies = (state: State) => state.movies;
 
 export const movieFromId = (movieId: Movie['id']) =>
-  (state: State): Movie | undefined =>
-    pipe(
-      state,
-      movies,
-      (movies) => movies[movieId]
-    );
+  createSelector(
+    movies,
+    movieList => movieList[movieId]
+  );
 
 export const actors = (state: State): State['actors'] => state.actors;
-
-export const actorFromId = (actorId: Actor['id']) =>
-  (state: State): Actor | undefined =>
-    pipe(
-      state,
-      actors,
-      actors => actors[actorId]
-    );
 
 const isActor = (t: Actor | undefined): t is Actor => !(t === undefined);
 
 export const actorsFromMovieId = (movieId: Movie['id']) =>
-  (state: State): Option<Actor[]> =>
-    pipe(
-      state,
-      movies,
-      moviesList => moviesList[movieId],
-      fromNullable,
-      mapOption(
-        movie => pipe(
-          movie.actors,
-          map(
-            actorId => pipe(
-              state,
-              actorFromId(actorId)
+  createSelector(
+    [movieFromId(movieId), actors],
+    (movie, actors) =>
+      pipe(
+        movie,
+        fromNullable,
+        mapOption(
+          movie =>
+            pipe(
+              movie.actors,
+              map(
+                actorId => actors[actorId]
+              ),
+              filter(isActor)
             )
-          ),
-          filter(isActor),
         )
       )
-    );
+  );
