e74e784d1c4a6f5ce96dc578cdec25ab1016309c
Victor Graffard <victor.graffard@iadvize.com>
2019-12-13 14:46:15 +0100

    feat(selectors): Now using reselect

--- src/selector.ts
+++ src/selector.ts
@@ -1,25 +1,23 @@
+import { createSelector } from 'reselect';
+
 import { pipe } from 'fp-ts/lib/pipeable';
 import { map, filter } from 'fp-ts/lib/Array';
-import { fromNullable, Option, map as mapOption } from 'fp-ts/lib/Option';
+import { fromNullable, map as mapOption } from 'fp-ts/lib/Option';
 
 import { Movie, State, Actor } from "./entities";
 
-export const movies = (state: State): State['movies'] => state.movies;
+export const movies = (state: State) => state.movies;
 
 export const movieFromId = (movieId: Movie['id']) =>
-  (state: State): Movie | undefined =>
-    pipe(
-      state,
-      movies,
-      (movies) => movies[movieId]
-    );
+  createSelector(
+    movies,
+    movieList => movieList[movieId]
+  );
 
 export const actors = (state: State): State['actors'] => state.actors;
 
 export const actorFromId = (actorId: Actor['id']) =>
-  (state: State): Actor | undefined =>
-    pipe(
-      state,
+    createSelector(
       actors,
       actors => actors[actorId]
     );
@@ -27,22 +25,22 @@ export const actorFromId = (actorId: Actor['id']) =>
 const isActor = (t: Actor | undefined): t is Actor => !(t === undefined);
 
 export const actorsFromMovieId = (movieId: Movie['id']) =>
-  (state: State): Option<Actor[]> =>
-    pipe(
-      state,
-      movies,
-      moviesList => moviesList[movieId],
-      fromNullable,
-      mapOption(
-        movie => pipe(
-          movie.actors,
-          map(
-            actorId => pipe(
-              state,
-              actorFromId(actorId)
+      createSelector(
+        state => state,
+        movieFromId(movieId),
+        (state, movie) => pipe(
+          movie,
+          fromNullable,
+          mapOption(
+            movie => pipe(
+              movie.actors,
+              map(
+                actorId => pipe(
+                  state,
+                  actorFromId(actorId)
+                )),
+                filter(isActor)
+              )
             )
-          ),
-          filter(isActor),
-        )
-      )
-    );
+          )
+      );
